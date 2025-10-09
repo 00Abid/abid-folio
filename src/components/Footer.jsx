c82a0c24b357ef793e0ffb50fc20d8e0c0110f4a
Khan Abid <kaabid1557@gmail.com>
2025-10-09 20:56:44 +0530

    Update Footer.jsx
    Fixed the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div className=" relative z-10 flex justify-center items-center h-auto w-[80%] border-t mx-auto mt-5">
             <h6 className="text-sm p-4 text-white">
-                &#xA9; {new Date().getFullYear()} ABID KHAN
+                &#xA9; {new Date().getFullYear()} ABID KHAN | Frontend Developer
             </h6>
         </div>
     )
