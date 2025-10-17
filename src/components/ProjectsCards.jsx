a53d30f8866aaabba91cd8ec23884a02ac59920b
00Abid <kaabid1557@gmail.com>
2025-10-17 21:17:47 +0530

    Updated on 17 Oct, Time: 9:17 PM

--- src/components/ProjectsCards.jsx
+++ src/components/ProjectsCards.jsx
@@ -13,7 +13,7 @@ const projects = [
         id: 2,
         title: "CaloriCurve - Nutrition Tracker",
         tech: "HTML · CSS · JS · Firebase",
-        desc: "Track daily food intake and calories with smart recommendations",
+        desc: "Track meals, calories, and nutrients daily with AI powered Food recognition and smart recommendations",
         image: "/ca.webp",
         link: "https://caloricurve.vercel.app/",
     },
